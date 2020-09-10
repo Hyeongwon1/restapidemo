b4ff300ed0826d869b2e93d2229ef1d7954322f2
Seo HyeongWon <32199485+Hyeongwon1@users.noreply.github.com>
2020-09-10 22:48:20 +0900

    parameters test case

--- rest-api/src/test/java/me/shw/restapi/events/EventTest.java
+++ rest-api/src/test/java/me/shw/restapi/events/EventTest.java
@@ -2,7 +2,14 @@ package me.shw.restapi.events;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class EventTest {
 	
@@ -30,7 +37,48 @@ public class EventTest {
 		
 	}
 	
-	   
-	
+	@ParameterizedTest
+	@CsvSource({
+		"0,0,true",
+		"100,0,false",
+		"0,100,true"
+		
+	})
+	public void testFree(int basePrice, int maxPrice, boolean isFree) {
+		//given
+		Event event = Event.builder()
+				.basePrice(basePrice)
+				.maxPrice(maxPrice)
+				.build()
+				;
+		//When
+		event.update();
 
+		//then
+		assertThat(event.isFree()).isEqualTo(isFree);
+				
+	}
+	
+	@ParameterizedTest
+	@MethodSource("prametersForTestoffline")
+	public void testoffline(String location, boolean isOffline) {
+		//given
+		Event event = Event.builder()
+					.location(location)
+					.build()
+					;
+		//When
+		event.update();
+
+		//then
+		assertThat(event.isOffline()).isEqualTo(isOffline);
+	}
+	
+	static Object [] prametersForTestoffline() {
+		return new Object[] {
+			new Object[] {"강남",true},
+			new Object[] {null,false},
+			new Object[] {"  ",false}
+		};
+	}
 }
